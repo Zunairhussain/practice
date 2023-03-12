15147615faed75d1a5e68bc719c598ee3b120582
Zunairhussain <zunairhussain065@gmail.com>
2023-03-11 20:12:25 -0800

    Practice

---
+++ hello.js
@@ -0,0 +1,22 @@
+var a= "Zunair Ansari Mallik"; //number
+console.log(a);
+var floatingData = 2.4
+var stringData = "How are you"
+var stringData2 = "how are you"
+
+var num = 4+4
+ console.log ('4+4',4+4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

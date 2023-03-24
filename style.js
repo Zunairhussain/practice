e62c977f210fb22c85efb0d4919b1903ae82ba8f
Zunairhussain <zunairhussain065@gmail.com>
2023-03-24 12:04:03 -0700

    video

---
+++ style.js
@@ -0,0 +1,37 @@
+// var a= "Zunair Ansari Mallik"; //number
+// console.log(a);
+// var floatingData = 2.4
+// var stringData = "How are you"
+// console.log (stringData)
+// var stringData2 = "how are you"
+// console.log (stringData2);
+
+// var num = 4+4
+//  console.log ('4+4',4+4)
+
+// const anasriBoy= "Zunair Ansari is a ";
+// const secondStr = "good Boy";
+// const concat = anasriBoy + secondStr;
+// console.log(concat);
+
+// var uses= prompt('ENTER YOUR DATA')
+// let gainmakrs += (ALi)
+// gainmarks = 
+//  ||
+
+
+// var userNumber=+ prompt( "Enter a positive number" )
+// if (userNumber >0) {
+//     alert(" Enter a positive number")
+// }
+
+
+// var userNumber=+ prompt("Enter a number" )
+
+// var userNumber2=+ prompt("Enter a number")
+
+// if (userNumber == userNumber2) {
+//     alert("Number are equal")
+// }
+
+
